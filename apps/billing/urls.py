a26d5125b783c0cd2153f355043fe750602963ec
rhysmcneill <rhysmcneill7@hotmail.co.uk>
2025-03-29 13:36:22 +0000

    [DAY 6]: Creating an individual bill viewer

--- apps/billing/urls.py
+++ apps/billing/urls.py
@@ -1,4 +1,4 @@
-from .views import create_bill_view, list_bill_view, delete_bill_view, update_bill_view
+from .views import create_bill_view, list_bill_view, delete_bill_view, update_bill_view, view_bill_view
 from django.urls import path
 
 urlpatterns = [
@@ -6,5 +6,5 @@ urlpatterns = [
     path('business/<slug:business_slug>/bills/new/', create_bill_view, name='create_bill'),
     path('business/<slug:business_slug>/bills/delete/<uuid:uuid>/', delete_bill_view, name='delete_bill'),
     path('business/<slug:business_slug>/bills/<uuid:uuid>/update/', update_bill_view, name='update_bill'),
-
+    path('business/<slug:business_slug>/bills/<uuid:uuid>/view/', view_bill_view, name='view_bill'),
 ]
